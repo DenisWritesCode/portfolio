c966463f0902b2c3e12cb3ebc8a688f72e1bf459
Dev Denis <deniswritescode@gmail.com>
2021-03-04 00:13:17 +0200

    Messed around with package.json to no avail. Need to watch CSS

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -16,10 +16,9 @@ function Resume() {
           </p>
         </div>
         <div class="cards text-gray-800 md:flex md:-mt-40 w-4/5 mx-auto">
-          
-          <div className="skill-container p-4 mb-20 pt-20 -mt-20">
-            <div class="skill shadow-xl md:w-1/3 md:m-3 p-3 bg-gray-50 rounded-xl">
-              <div className="skill-content relative flex-col items-center mb-2">
+          <div className="skill-container p-4 mb-20 pt-20 -mt-16">
+            <div class="skill">
+              <div className="skill-content relative flex-col lg:items-center mb-2">
                 <div className="img -mt-20">
                   <img
                     src="https://images.unsplash.com/photo-1564865878688-9a244444042a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
@@ -43,8 +42,8 @@ function Resume() {
             </div>
           </div>
 
-          <div className="skill-container p-4 mb-20 pt-20 -mt-20">
-            <div class="skill shadow-xl md:w-1/3 md:m-3 p-3 bg-gray-50 rounded-xl">
+          <div className="skill-container p-4 mb-20 pt-20 -mt-16">
+            <div class="skill">
               <div className="skill-content relative flex-col items-center mb-2">
                 <div className="img -mt-20">
                   <img
@@ -55,7 +54,7 @@ function Resume() {
                 </div>
                 <div className="text m-2 top-2">
                   <p className="text-3xl font-semibold text-gray-800 text-center">
-                    React
+                    Tailwind
                   </p>
                   <p className="text-lg">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. A
@@ -69,8 +68,8 @@ function Resume() {
             </div>
           </div>
 
-          <div className="skill-container border-2 border-green-500 mb-3 p-4 pt-20 -mt-20">
-            <div class="skill shadow-xl md:w-1/3 md:m-3 p-3 bg-gray-50 rounded-xl">
+          <div className="skill-container mb-3 p-4 pt-20 -mt-16">
+            <div class="skill">
               <div className="skill-content relative flex-col items-center mb-2">
                 <div className="img -mt-20">
                   <img
@@ -81,7 +80,7 @@ function Resume() {
                 </div>
                 <div className="text m-2 top-2">
                   <p className="text-3xl font-semibold text-gray-800 text-center">
-                    React
+                    Python
                   </p>
                   <p className="text-lg">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. A
@@ -94,7 +93,6 @@ function Resume() {
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </section>
