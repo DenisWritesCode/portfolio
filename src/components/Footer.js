6d19036a4017947c426ad1b3bf57a60ce26fef48
Dev Denis <deniswritescode@gmail.com>
2021-03-07 20:04:33 +0200

    Footer is Mobile Ready

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="bg-gray-900 text-gray-300 text-lg flex flex-col items-center py-3 font-semibold tracking-wider">
+      <p><a href="#hero">Back To Top</a></p>
+      <p className="">DenisWritesCode &#169; 2021</p>
+    </footer>
+  );
+}
+
+export default Footer;
