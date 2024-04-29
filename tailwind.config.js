7c794a8c7a06e0f1d0fe555cc8624b04a50955fe
DenisWritesCode <DWC@Denis-MBP.local>
2024-04-29 11:59:48 +0300

    self reviewed PR

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,3 @@
-// export const purge = ;
-// export const darkMode = false;
-// export const theme = {
-//     extend: {},
-// };
-// export const variants = {
-//     extend: {},
-// };
-// export const plugins = [];
-
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
